4221ffe391af981f4d7a46aa3dabb53c0d8b7730
Septickiller <ganciandrei@gmail.com>
2012-06-25 22:07:17 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Academy::Application.config.secret_token = 'd11f92984d84ec2e03ee6d6890b55abaab521f97450992a67a8961bf7e6a44ab9812b3be12e88fa08ec10130f80ed2a4a85ef310186b3f1a81ae34e797397073'
